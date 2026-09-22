7bb114330fe59d92b34f579b6e10ff66dd280a17
Bryan Fisher <bfisher@eou.edu>
2026-09-22 11:11:35 -0700

    modified:   net.pl 	new file:   zononet-1.pdf 	new file:   zononet.1 	new file:   zononet.log 	modified:   zononet.mp

--- net.pl
+++ net.pl
@@ -1,7 +1,9 @@
+#  Converts unfolded polyhedron (net) produced by Antiprism into
+#  a metapost file, suitable for customization.
+
 use v5.40;
 
 my @vertices = ();
-my @faces = ();
 
 my $infile = 'zononet.off';
 my $outfile = 'zononet.mp';
@@ -9,38 +11,39 @@ my $outfile = 'zononet.mp';
 open(my $fh1, '<', $infile) or die "Could not open '$infile': $!";
 open(my $fh2, '>', $outfile) or die "Could not open '$outfile': $!";
 
+print $fh2 "numeric u;\n";
+print $fh2 "u = 5pt;\n";
+print $fh2 "beginfig(1);\n";
+print $fh2 "pickup pencircle scaled 0.1pt;\n";
+print $fh2 "\n";
+
 while (my $line = <$fh1>) {
-  next if $. <= 2;
-  chomp $line;
-  if (2 < $. and $. <= 1744) {
+  if ($. >= 3 && $. <= 1744) {
+    chomp $line;
     my ($x, $y) = $line =~ /(-?\d+\.\d+)\s+(-?\d+\.\d+)\s+(-?\d+\.\d+)/;
     push @vertices, [$x, $y]; 
   }
-  if ($. >= 1745) {
+  if ($. > 1744) {
+    chomp $line;
     my ($vs, $vn1, $vn2, $vn3, $vn4) = $line =~ /(\d+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)/;
-    push @faces, [$vertices[$vn1], $vertices[$vn2], $vertices[$vn3], $vertices[$vn4]];
-    #say join ' ', @{$vertices[$vn1]};
+
+    print $fh2 "draw (";
+    print $fh2 join ", ", @{$vertices[$vn1]};
+    print $fh2 ")--(";
+    print $fh2 join ", ", @{$vertices[$vn2]};
+    print $fh2 ")--(";
+    print $fh2 join ", ", @{$vertices[$vn3]};
+    print $fh2 ")--(";
+    print $fh2 join ", ", @{$vertices[$vn4]};
+    print $fh2 ")--cycle;\n";
+
   }
 }
 
+print $fh2 "endfig;\n";
+print $fh2 "end";
+
 close($fh1);
-
-print $fh2 "numeric u;\n";
-print $fh2 "u = 1pt;\n";
-print $fh2 "\n";
-
-
-for my $n (1..scalar @faces);
-
-my (
-print $fh2 "draw (", $vertices[$faces[$n][1], ",", 
-                     $vertices[$faces[$n][2], ",",
-                     $vertices[$faces[$n][3], ",",
-                     $vertices[$faces[$n][4] ");";
-
-
-
 close($fh2);
 
 say scalar @vertices;
-say scalar @faces;
